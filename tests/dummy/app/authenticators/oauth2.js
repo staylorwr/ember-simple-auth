73cf1a7b1f93f5fdb0c05d4a000268a97f63b1d8
Scott Taylor <scott@motel.is>
2016-06-15 15:20:21 -0500

    fix dummy app API Endpoints

--- tests/dummy/app/authenticators/oauth2.js
+++ tests/dummy/app/authenticators/oauth2.js
@@ -1,5 +1,6 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenRevocationEndpoint: '/revoke'
+  serverTokenEndpoint: 'http://localhost:4200/token',
+  serverTokenRevocationEndpoint: 'http://localhost:4200/revoke'
 });
